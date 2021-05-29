3694e57f274c08434dfdfc691b0585285c880afb
LJH <scoke0801@gmail.com>
2021-05-29 20:26:06 +0900

    save

--- GSClient/GSClient/Player.cpp
+++ GSClient/GSClient/Player.cpp
@@ -140,12 +140,14 @@ void CPlayer::FixCameraByTerrain(CTerrain* pTerrain)
 void CPlayer::FixPositionByTerrain(CTerrain* pTerrain)
 {
 	if (m_isOnGround) {
+		auto aabb = m_AABB[0]->GetBox();
+		
 		float heightCenter = pTerrain->GetDetailHeight(m_xmf3Position.x, m_xmf3Position.z);
-		float heightLT = pTerrain->GetDetailHeight(m_xmf3Position.x - 150, m_xmf3Position.z + 100);
-		float heightLB = pTerrain->GetDetailHeight(m_xmf3Position.x - 150, m_xmf3Position.z - 100);
-		float heightRT = pTerrain->GetDetailHeight(m_xmf3Position.x + 150, m_xmf3Position.z + 100);
-		float heightRB = pTerrain->GetDetailHeight(m_xmf3Position.x + 150, m_xmf3Position.z - 100);
-		m_xmf3Position.y = (heightCenter + heightLT + heightLB + heightRT + heightRB) / 5.0f + 5; 
+		float heightLT = pTerrain->GetDetailHeight(m_xmf3Position.x - aabb.Extents.x, m_xmf3Position.z + aabb.Extents.z);
+		float heightLB = pTerrain->GetDetailHeight(m_xmf3Position.x - aabb.Extents.x, m_xmf3Position.z - aabb.Extents.z);
+		float heightRT = pTerrain->GetDetailHeight(m_xmf3Position.x + aabb.Extents.x, m_xmf3Position.z + aabb.Extents.z);
+		float heightRB = pTerrain->GetDetailHeight(m_xmf3Position.x + aabb.Extents.x, m_xmf3Position.z - aabb.Extents.z);
+		m_xmf3Position.y = (heightCenter + heightLT + heightLB + heightRT + heightRB) / 5.0f;
 		//m_xmf3Position.y = pTerrain->GetDetailHeight(m_xmf3Position.x, m_xmf3Position.z);
 	}
 }
