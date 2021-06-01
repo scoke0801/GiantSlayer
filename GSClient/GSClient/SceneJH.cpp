25dabba952b1f3c78f0682c8ec7c67e862162d36
LJH <scoke0801@gmail.com>
2021-06-01 11:26:18 +0900

    model_testing

--- GSClient/GSClient/SceneJH.cpp
+++ GSClient/GSClient/SceneJH.cpp
@@ -2912,7 +2912,7 @@ void CSceneJH::BuildArrows(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* 
 void CSceneJH::BuildPlayers(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList)
 {
 	CGameObjectVer2* pKinght = CGameObjectVer2::LoadGeometryAndAnimationFromFile(pd3dDevice, pd3dCommandList,
-		m_pd3dGraphicsRootSignature, "resources/FbxExported/Knight.bin", NULL, true); 
+		m_pd3dGraphicsRootSignature, "resources/FbxExported/Player.bin", NULL, true); 
 	
 	m_Players[0] = new CPlayer(pd3dDevice, pd3dCommandList);
 	m_Player = m_Players[0];
